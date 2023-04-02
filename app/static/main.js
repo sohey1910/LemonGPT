a0f91ced2b2acae4a3c1e262dd192471299e2885
sohey1910 <soheyi@163.com>
2023-04-02 18:09:38 +0800

    mod main.js

--- app/static/main.js
+++ app/static/main.js
@@ -24,25 +24,23 @@ $(document).ready(function(){
 			type: 'POST',
 			data: {question: question,id:id},
 			success: function(data){
-				$('.chatlogs').append('<p class="bot-answer">' + data.answer + '</p>');
+				var source = new EventSource("/streaming?id="+id);
+				// sse 连接开启时回调函数
+				source.onopen = function (event) {
+					console.log('EventSource.readyState ' + source.readyState);
+				}
+				// 消息监听，event 是后端返回的数据,相当于python字典
+				source.onmessage = function (event) {
+					// update_data(event);
+					console.log(event.data);
+				}
+				source.onerror = function (event) {
+					source.close();
+					console.log('EventSource.readyState ' + source.readyState);
+				}
+				// $('.chatlogs').append('<p class="bot-answer">' + data.answer + '</p>');
 			}
 		});
-
-		var source = new EventSource("/streaming?id="+id);
-		// sse 连接开启时回调函数
-		source.onopen = function (event) {
-			console.log('EventSource.readyState ' + source.readyState);
-		}
-		// 消息监听，event 是后端返回的数据,相当于python字典
-		source.onmessage = function (event) {
-			// update_data(event);
-			console.log(event.data);
-		}
-		source.onerror = function (event) {
-			source.close();
-			console.log('EventSource.readyState ' + source.readyState);
-		}
-
 	});
 
 	$.ajax({
